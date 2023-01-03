2a241785247b5a60480cb66cfd9c5fe1aeb6fbb7
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-01-03 17:43:53 +0100

    list of student success

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;
