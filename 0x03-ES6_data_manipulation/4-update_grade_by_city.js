c1233a73843fd9d268ae9f29a2012ca26840a2bc
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-01-04 21:54:41 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,22 @@
-// 4-update_grade_by_city.js
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  // Filter students by location
+  // Change/add grade for students in newGrades
+  // If a filtered student is not in new Grades => grade = N/A
+
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item2 = newGrades.find((student2) => student.id === student2.studentId);
+    // });
+    // console.log('FOUND STUDENT1: ', student);
+    // console.log('FOUND ITEM2: ', item2);
+    // console.log(item2 ? {...student, ...item2} : student.grade = 'N/A', student);
+    // if (!item2) {
+    //   student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+    // }
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
+    // return item2 ? {...student, ...item2} : student,  student.grade = 'M/A';
+  });
+  return cityStudents;
+}
